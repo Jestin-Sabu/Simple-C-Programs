6d8d98ea4a9349a68f13a60acb15624a70a164b7
Jestin Sabu <jestinsabu721@gmail.com>
2020-05-02 10:57:07 +0530

    Added reverse_store_array.c

---
+++ reverse_store_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<malloc.h>
+
+int main(void)
+{
+  int n, i, j, temp;
+  int *p;
+  printf("Enter the no. of Elements : ");
+  scanf("%d", &n);
+  p = (int*)malloc(n*sizeof(int));
+  printf("Enter the elements : ");
+  for(i=0;i<n;i++)
+    scanf("%d", p+i);
+  for(i=0,j=n-1;i<n/2;i++,j--)
+  {
+    temp = *(p+i);
+    *(p+i) = *(p+j);
+    *(p+j) = temp;
+  }
+  printf("Reversed Array : ");
+  for(i=0;i<n;i++)
+    printf("%d ", *(p+i));
+  return 0;
+}
