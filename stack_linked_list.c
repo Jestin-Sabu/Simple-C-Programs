a1af855b39da3a81ed25c1ec0af31078209b318e
Jestin Sabu <jestinsabu721@gmail.com>
2020-11-10 11:59:40 +0530

    Add stack_linked_list.c

---
+++ stack_linked_list.c
@@ -0,0 +1,86 @@
+//Program to implement stack operations using linked list
+
+#include<stdio.h>
+#include<malloc.h>
+
+typedef struct stack{
+  struct stack* NEXT;
+  int DATA;
+}stack_t;
+
+stack_t* createnode(){
+  stack_t* temp = malloc(sizeof(temp));
+  temp->NEXT = NULL;
+  return temp;
+}
+
+void push(stack_t* HEAD){
+  int temp;
+  printf("   Enter the integer data to insert : ");
+  scanf("%d", &temp);
+  stack_t* new = createnode();
+  new->DATA = temp;
+  new->NEXT = HEAD->NEXT;
+  HEAD->NEXT = new;
+}
+
+void pop(stack_t* HEAD){
+  stack_t* ptr = HEAD->NEXT;
+  if(ptr == NULL){
+    printf("   Empty stack, failed to pop\n");
+    return;
+  }
+  printf("   Popped Item = %d\n", ptr->DATA);
+  HEAD->NEXT = ptr->NEXT;
+  free(ptr);
+}
+
+void print(stack_t* HEAD){
+  stack_t* ptr = HEAD->NEXT;
+  printf("   ");
+  while(ptr != NULL){
+    printf("%d ", ptr->DATA);
+    ptr = ptr->NEXT;
+  }
+}
+
+void freestack(stack_t* HEAD){
+  stack_t* temp;
+  stack_t* ptr = HEAD->NEXT;
+  free(HEAD);
+  while(ptr != NULL){
+    temp = ptr;
+    ptr = ptr->NEXT;
+    free(temp);
+  }
+}
+
+int main(void){
+  int flag = 1, ch;
+  stack_t* HEAD = createnode();
+  while(flag){
+      printf("\nStack Operations");
+      printf("\n^^^^^^^^^^^^^^^^\n");
+      printf("1. Push into Stack\n");
+      printf("2. Pop out of Stack\n");
+      printf("3. View Stack\n");
+      printf("4. Exit\n");
+      printf("   Enter your choice : ");
+      scanf("%d", &ch);
+      switch(ch){
+          case 1: push(HEAD);
+                  print(HEAD);
+                  break;
+          case 2: pop(HEAD);
+                  print(HEAD);
+                  break;
+          case 3: print(HEAD);
+                  break;
+          case 4: flag = 0;
+                  freestack(HEAD);
+                  break;
+          default: printf("Wrong Choice !!!");
+      }
+    }
+  return 0;
+}
